8ff2b6dbd8a25c774e0f9106997692bc51edd8de
lespodsay <lespodsay@gmail.com>
2025-07-21 18:06:37 +0300

    merge

--- src/About.js
+++ src/About.js
@@ -5,19 +5,19 @@ const About = () => {
             <h2>About Le Spodsay</h2>
             <div className='container about-page'>
                 <p>
-                Hello, I am Le Spodsay! I am a 22 year old computer engineering student 
+                Hello, I am Le Spodsay! I am a 23 year old computer engineering student 
                 based in Istanbul, Turkey.
                 <br />My journey as an artist began when I was 2 years old
                 the day my pre-school teacher passed out a drawing journal, and I've been drawing ever since.
                 I draw inspiration from the many things I consume such as anime, manga, video games,
-                and other media. I aspire to become a game developer, building my own games,
+                and other media. It is my dream to get into game development, to build my own games,
                 from basic mechanisms and game levels to character design and publishing. In the mean time,
                 I will continue to make art that is both beautiful and fulfilling.
 
                 </p>
                 <br />
                 <p>
-                Find me on: <a href="https://www.twitter.com/lespodsay">Twitter</a> | <a href="https://www.instagram.com/lespodsay">Instagram</a> | <a href="https://www.tiktok.com/@lespodsay">Tiktok</a> |  <a href="https://cara.app/lespodsay">Cara</a>
+                Find me on: <a href="https://bsky.app/profile/lespodsay.bsky.social">BluSky</a> | <a href="https://www.twitter.com/lespodsay">Twitter</a> | <a href="https://www.instagram.com/lespodsay">Instagram</a> | <a href="https://www.tiktok.com/@lespodsay">Tiktok</a> |  <a href="https://cara.app/lespodsay">Cara</a>
                 <br />
                 Email: <a href="mailto:lespodsay@gmail.com">lespodsay@gmail.com</a>
                 </p>
