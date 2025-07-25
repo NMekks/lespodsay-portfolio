1ecb586a6a6313e887b52a733d042adb243edbdc
lespodsay <lespodsay@gmail.com>
2025-07-25 19:36:07 +0300

    updated Home component to follow best practice image display

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,15 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+    const [homeIllustrations, setHomeIllustrations] = useState([]);
+
+
+    useEffect(() => {
+        fetch("/homePageIllustrationsData.json")
+          .then((res) => res.json())
+          .then((data) => setHomeIllustrations(data));
+      }, []);
+
     return (
         <div className="home">
 
@@ -6,97 +17,17 @@ const Home = () => {
         <main>
             <div className="container home">
             <div className="gallery">
+            {homeIllustrations.map((art, idx) => (
+          <div className="img" key={idx}>
             <a
-                href="img/magicalMiraiMiku2024.jpeg"
-                data-lightbox="artwork"
-                data-title="Magical Mirai Miku 2024"
-                >
-                <img src="img/magicalMiraiMiku2024.jpeg" />
-                </a>
-                <a
-                href="img/Summer Namine artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Summer Namine by Lespodsay"
-                >
-                <img src="img/Summer Namine artpiece.jpeg" />
-                </a>
-                <a
-                href="img/GENSHIN Xiao artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Genshin Impact Xiao by Lespodsay"
-                >
-                <img src="img/GENSHIN Xiao artpiece.jpeg" />
-                </a>
-                <a
-                href="img/GENSHIN venti artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Genshin Impact Venti by Lespodsay"
-                >
-                <img src="img/GENSHIN venti artpiece.jpeg" />
-                </a>
-                <a
-                href="img/GENSHIN layla artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Genshin Impact Layla by Lespodsay"
-                >
-                <img src="img/GENSHIN layla artpiece.jpeg" />
-                </a>
-                <a
-                href="img/KH riku artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Kingdom Hearts Riku by Lespodsay"
-                >
-                <img src="img/KH riku artpiece.jpeg" />
-                </a>
-                <a
-                href="img/KH kairi artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Kingdom Hearts Kairi by Lespodsay"
-                >
-                <img src="img/KH kairi artpiece.jpeg" />
-                </a>
-                <a
-                href="img/OP perona artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="One Piece Perona by Lespodsay"
-                >
-                <img src="img/OP perona artpiece.jpeg" />
-                </a>
-                <a
-                href="img/TLOZ link artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="The Legend Of Zelda Link by Lespodsay"
-                >
-                <img src="img/TLOZ link artpiece.jpeg" />
-                </a>
-                <a
-                href="img/WITCH will artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="W.I.T.C.H. Will Vandom by Lespodsay"
-                >
-                <img src="img/WITCH will artpiece.jpeg" />
-                </a>
-                <a
-                href="img/JJK gojo artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Jujutsu Kaisen Gojo Satoru by Lespodsay"
-                >
-                <img src="img/JJK gojo artpiece.jpeg" />
-                </a>
-                <a
-                href="img/CCS Sakura artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Cardcaptor Sakura by Lespodsay"
-                >
-                <img src="img/CCS Sakura artpiece.jpeg" />
-                </a>
-                <a
-                href="img/KH namine artpiece.jpeg"
-                data-lightbox="artwork"
-                data-title="Kingdom Hearts Namine by Lespodsay"
-                >
-                <img src="img/KH namine artpiece.jpeg" />
-                </a>
+              href={art.imagePath}
+              data-lightbox="artwork"
+              data-title={art.caption}
+            >
+              <img src={art.imagePath} alt={art.alt || art.title} />
+            </a>
+          </div>
+        ))}
             </div>
             </div>
             
