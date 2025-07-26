e8008075c48bd178e50af092ca57253c4e448357
lespodsay <lespodsay@gmail.com>
2025-07-26 19:05:52 +0300

    fixed contact page margins, fixed footer copyrights, reverted back to previous half-broken filtering and image display, updated logo image, added and removed some styling in css sheet

--- src/Illustration.js
+++ src/Illustration.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import IllustrationSearch from "./IllustrationSearch";
+ 
 
 const Illustrations = () => {
   const [illustrations, setIllustrations] = useState([]);
@@ -14,10 +15,12 @@ const Illustrations = () => {
     <div className="illustration-container">
       <div id="banner" />
       <div class="container-fluid mt-5">
-            { <div className="illustration-search-dropdown"><IllustrationSearch /> </div>}
-          </div>
+        <div className="illustration-search-dropdown">
+          {<IllustrationSearch />}
+        </div>
+      </div>
       <div className="illustration-gallery">
-        {illustrations.map((art, idx) => (
+        { illustrations.map((art, idx) => (
           <div className="img" key={idx}>
             <a
               href={art.imagePath}
